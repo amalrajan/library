91ba2586fac3898ccd8839336af9574a30c72373
Amal Rajan <amalrajan74@gmail.com>
2020-04-09 19:35:02 +0530

    Update selection_sort.cpp

--- sorting/selection_sort.cpp
+++ sorting/selection_sort.cpp
@@ -8,14 +8,32 @@ using namespace std;
 int arr[100005];
 
 
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int mini = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[mini]) {
+                mini = j;
+            }
+        }
+        swap(arr[mini], arr[i]);
+    }
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+
+    const int n = 5;
+    int arr[n] = {7, 2, 0, 2, 5};
+
+    selection_sort(arr, n);
+    for (int i = 0; i < n; i++) cout << arr[i] << ' ';
 
     return 0;
 }
