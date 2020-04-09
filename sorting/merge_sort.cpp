35c555f164014aa0f9372873b8e403a1e5a3449a
Amal Rajan <amalrajan74@gmail.com>
2020-04-09 19:45:14 +0530

    Update merge_sort.cpp

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,22 +1,55 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+#define ll long long
+#define watch(x) cout << (#x) << " is " << (x) << endl
+
 using namespace std;
 
-void merge(int arr[], int start, int mid, int end) {
-    int p = start;
-    int q = mid + 1;
+int temp[100005];
 
-    int temp[end-start+1];
-    int k = 0;
 
-    for (int i = start; i <= end; i++) {
-        if (p > mid)
-            temp[k++] = arr[p++];
-        else if (q > end)
-            temp[k++] = arr[q++];
-        else if ()
+void merge_sort(int*, int, int, int);
+void merge(int*, int, int, int);
+
+
+void merge_sort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
 
-int main() {
+void merge(int arr[], int left, int mid, int right) {
+    int i = left, j = mid + 1, k = left;
     
+    while (i <= mid && j <= right) {    
+        if (arr[i] < arr[j]) temp[k++] = arr[i++];
+        else temp[k++] = arr[j++];
+    }
+
+    while (i <= mid) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
+
+    for (int i = left; i <= right; i++) arr[i] = temp[i];
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+
+    const int n = 5;
+    int arr[n] = {7, 2, 0, 2, 5};
+
+    merge_sort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) cout << arr[i] << ' ';
+
+    return 0;
 }
