07bbedecbd7e989174a174307e974523533fc4af
Amal Rajan <amalrajan74@gmail.com>
2020-05-21 20:28:54 +0530

    Encapsulate functions

--- graph/dsu.py
+++ graph/dsu.py
@@ -1,25 +1,29 @@
-def dsu_root(i):
-    while arr[i] != i:
-        i = arr[arr[i]]
-    return i
+class DSU:
+    def __init__(self, n):
+        self.arr = list(range(1, n + 1))
+        self.szarr = [1] * n
+
+    def dsu_root(i):
+        while self.arr[i] != i:
+            i = self.arr[self.arr[i]]
+        return i
 
 
-def dsu_find(a, b):
-    return dsu_root(a) == dsu_root(b)
+    def dsu_find(a, b):
+        return dsu_root(a) == dsu_root(b)
 
 
-def dsu_weighted_union(a, b):
-    root_a = dsu_root(a)
-    root_b = dsu_root(b)
+    def dsu_weighted_union(a, b):
+        root_a = dsu_root(a)
+        root_b = dsu_root(b)
 
-    if szarr[root_a] < szarr[root_b]:
-        arr[root_a] = arr[root_b]
-        szarr[root_b] += szarr[root_a]
-    else:
-        arr[root_b] = arr[root_a]
-        szarr[root_a] += szarr[root_b]
+        if self.szarr[root_a] < self.szarr[root_b]:
+            self.arr[root_a] = self.arr[root_b]
+            self.szarr[root_b] += self.szarr[root_a]
+        else:
+            self.arr[root_b] = self.arr[root_a]
+            self.szarr[root_a] += self.szarr[root_b]
 
 
-n = 10
-arr = list(range(1, n + 1))
-szarr = [1] * 10
+dsu = DSU(10)
+print(dsu.arr)
